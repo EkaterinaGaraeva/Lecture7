5342dce792451c800ad5ac852b6bf8f5a384a42a
Ваше имя <garaevaea@mail.ru>
2022-04-14 03:55:40 +0300

    Add Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,30 @@
+﻿// 1. Обход директории 
+
+/*
+string path = // путь к папке
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+*/
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+
+string path = @"C:\Users\Asus\Desktop\Разработчик\004 - Знакомство с языками программирования\Лекция 1";
+CatalogInfo(path);
